c8880b5453e208e9dfeb953229f53f5ed60dfe7f
REGROUP\michele.alaia <michele.alaia@relatech.com>
2025-07-29 14:16:07 +0200

    firstcommit

---
+++ src/main/java/com/alaia/pharmX/PharmXApplication.java
@@ -0,0 +1,13 @@
+package com.alaia.pharmX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alaia/pharmX/PharmXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaia.pharmX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
